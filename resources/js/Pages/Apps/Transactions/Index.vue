54af7fea24a20ab39f3a0a902ed6c3bc49034d31
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-07-22 21:20:54 +0700

    Menampilkan Item di Carts

---
+++ resources/js/Pages/Apps/Transactions/Index.vue
@@ -0,0 +1,257 @@
+<template>
+
+    <Head>
+        <title>Transactions - Aplikasi Kasir</title>
+    </Head>
+    <main class="c-main">
+        <div class="container-fluid">
+            <div class="fade-in">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card border-0 rounded-3 shadow">
+                            <div class="card-body">
+
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text"><i class="fa fa-barcode"></i></span>
+                                    <input type="text" class="form-control" v-model="barcode" @keyup="searchProduct"
+                                        placeholder="Scan or Input Barcode">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fw-bold">Product Name</label>
+                                    <input type="text" class="form-control" :value="product.title"
+                                        placeholder="Product Name" readonly>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label fw-bold">Qty</label>
+                                    <input type="number" class="form-control text-center" v-model="qty"
+                                        placeholder="Qty" min="1">
+                                </div>
+                                <div class="text-end">
+                                    <button @click.prevent="clearSearch"
+                                        class="btn btn-warning btn-md border-0 shadow text-uppercase mt-3 me-2"
+                                        :disabled="!product.id">CLEAR</button>
+                                    <button @click.prevent="addToCart"
+                                        class="btn btn-success btn-md border-0 shadow text-uppercase mt-3"
+                                        :disabled="!product.id">ADD ITEM</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+
+                        <div v-if="session.error" class="alert alert-danger">
+                            {{ session.error }}
+                        </div>
+
+                        <div v-if="session.success" class="alert alert-success">
+                            {{ session.success }}
+                        </div>
+
+                        <div class="card border-0 rounded-3 shadow border-top-success">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4 col-4">
+                                        <h4 class="fw-bold">GRAND TOTAL</h4>
+                                    </div>
+                                    <div class="col-md-8 col-8 text-end">
+                                        <h4 class="fw-bold">Rp. {{ formatPrice(grandTotal) }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card border-0 rounded-3 shadow">
+                            <div class="card-body">
+                                <div class="row mb-3">
+                                    <div class="col-md-6">
+                                        <label class="fw-bold">Cashier</label>
+                                        <input class="form-control" type="text" :value="auth.user.name" readonly>
+                                    </div>
+                                    <div class="col-md-6 float-end">
+                                        <label class="fw-bold">Customer</label>
+                                        <VueMultiselect v-model="customer_id" label="name" track-by="name"
+                                            :options="customers"></VueMultiselect>
+                                    </div>
+                                </div>
+                                <hr>
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr style="background-color: #e6e6e7;">
+                                            <th scope="col">#</th>
+                                            <th scope="col">Product Name</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">Qty</th>
+                                            <th scope="col">Sub Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="cart in carts" :key="cart.id">
+                                            <td class="text-center">
+                                                <button class="btn btn-danger btn-sm rounded-pill"><i
+                                                        class="fa fa-trash"></i></button>
+                                            </td>
+                                            <td>{{ cart.product.title }}</td>
+                                            <td>Rp. {{ formatPrice(cart.product.sell_price) }}</td>
+                                            <td class="text-center">{{ cart.qty }}</td>
+                                            <td class="text-end">Rp. {{ formatPrice(cart.price) }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="4" class="text-end fw-bold" style="background-color: #e6e6e7;">
+                                                TOTAL</td>
+                                            <td class="text-end fw-bold" style="background-color: #e6e6e7;">Rp. {{
+                                                formatPrice(carts_total) }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <hr>
+                                <div class="d-flex align-items-end flex-column bd-highlight mb-3">
+                                    <div class="mt-auto bd-highlight">
+                                        <label>Discount (Rp.)</label>
+                                        <input type="number" class="form-control" placeholder="Discount (Rp.)">
+                                    </div>
+                                    <div class="bd-highlight mt-4">
+                                        <label>Pay (Rp.)</label>
+                                        <input type="number" class="form-control" placeholder="Pay (Rp.)">
+                                    </div>
+                                </div>
+                                <div class="text-end mt-4">
+                                    <button
+                                        class="btn btn-warning btn-md border-0 shadow text-uppercase me-2">Cancel</button>
+                                    <button class="btn btn-purple btn-md border-0 shadow text-uppercase">Pay Order &
+                                        Print</button>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+
+//import layout
+import LayoutApp from '../../../Layouts/App.vue';
+
+//import Heade from Inertia
+import { Head, router } from '@inertiajs/vue3';
+
+//import VueMultiselect
+import VueMultiselect from 'vue-multiselect';
+import 'vue-multiselect/dist/vue-multiselect.css';
+
+//import ref form vue
+import { ref } from 'vue';
+
+//import axios
+import axios from 'axios';
+
+export default {
+    //layout
+    layout: LayoutApp,
+
+    //register components
+    components: {
+        Head,
+        VueMultiselect
+    },
+
+    //props
+    props: {
+        auth: Object,
+        customers: Array,
+        carts_total: Number,
+        session: Object,
+        carts: Array
+    },
+
+    //composition API
+    setup(props) {
+
+        //define state
+        const barcode = ref('');
+        const product = ref({});
+        const qty = ref(1);
+
+        //metho "searchProduct"
+        const searchProduct = () => {
+
+            //fetch with axios
+            axios.post('/apps/transactions/searchProduct', {
+
+                //send data "barcode"
+                barcode: barcode.value
+
+            }).then(response => {
+                if (response.data.success) {
+
+                    //assign response to state "product"
+                    product.value = response.data.data;
+                } else {
+
+                    //set state "product" to empty object
+                    product.value = {};
+                }
+            });
+        }
+
+        //method "clearSearch"
+        const clearSearch = () => {
+
+            //set state "product" to empty object
+            product.value = {};
+
+            //set state "barcode" to empty string
+            barcode.value = '';
+        }
+
+        //define state grandTotal
+        const grandTotal = ref(props.carts_total);
+
+        //method add to cart
+        const addToCart = () => {
+
+
+            //send data to server
+            router.post('/apps/transactions/addToCart', {
+
+                //data
+                product_id: product.value.id,
+                qty: qty.value,
+                sell_price: product.value.sell_price,
+
+            }, {
+                onSuccess: () => {
+
+                    //call method "clearSaerch"
+                    clearSearch();
+
+                    //set qty to "1"
+                    qty.value = 1;
+
+                    //update state "grandTotal"
+                    grandTotal.value = props.carts_total;
+                },
+            });
+
+
+        }
+
+        return {
+            barcode,
+            product,
+            searchProduct,
+            clearSearch,
+            qty,
+            grandTotal,
+            addToCart
+        }
+
+    }
+}
+</script>
+
+<style></style>
