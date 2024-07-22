5d8eb795886d0445719b8d33f5056e06b921c730
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2024-07-22 09:54:28 +0700

    CRUD Data Customers

---
+++ resources/js/Pages/Apps/Customers/Edit.vue
@@ -0,0 +1,142 @@
+<template>
+
+    <Head>
+        <title>Edit Customer - Aplikasi Kasir</title>
+    </Head>
+    <main class="c-main">
+        <div class="container-fluid">
+            <div class="fade-in">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card border-0 rounded-3 shadow border-top-purple">
+                            <div class="card-header">
+                                <span class="font-weight-bold"><i class="fa fa-user-circle"></i> EDIT CUSTOMER</span>
+                            </div>
+                            <div class="card-body">
+
+                                <form @submit.prevent="submit">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="mb-3">
+                                                <label class="fw-bold">Tipe</label>
+                                                <input class="form-control" v-model="form.name"
+                                                    :class="{ 'is-invalid': errors.name }" type="text"
+                                                    placeholder="Full Name">
+                                            </div>
+                                            <div v-if="errors.name" class="alert alert-danger">
+                                                {{ errors.name }}
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="mb-3">
+                                                <label class="fw-bold">No. Telp</label>
+                                                <input class="form-control" v-model="form.privilege"
+                                                    :class="{ 'is-invalid': errors.privilege }" type="number"
+                                                    placeholder="No. Telp">
+                                            </div>
+                                            <div v-if="errors.privilege" class="alert alert-danger">
+                                                {{ errors.privilege }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label class="fw-bold">Description</label>
+                                        <textarea class="form-control" v-model="form.description"
+                                            :class="{ 'is-invalid': errors.description }" type="text" rows="4"
+                                            placeholder="Description"></textarea>
+                                    </div>
+                                    <div v-if="errors.description" class="alert alert-danger">
+                                        {{ errors.description }}
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <button class="btn btn-primary shadow-sm rounded-sm"
+                                                type="submit">UPDATE</button>
+                                            <button class="btn btn-warning shadow-sm rounded-sm ms-3"
+                                                type="reset">RESET</button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+//import layout App
+import LayoutApp from '../../../Layouts/App.vue';
+
+//import Heade and Link from Inertia
+import { Head, Link, router } from '@inertiajs/vue3';
+
+//import reactive from vue
+import { reactive } from 'vue';
+
+//import sweet alert2
+import Swal from 'sweetalert2';
+
+export default {
+    //layout
+    layout: LayoutApp,
+
+    //register components
+    components: {
+        Head,
+        Link
+    },
+
+    //props
+    props: {
+        customer: Object,
+        errors: Object
+    },
+
+    //composition API
+    setup(props) {
+
+        //define form with reactive
+        const form = reactive({
+            name: props.customer.name,
+            privilege: props.customer.privilege,
+            description: props.customer.description
+        });
+
+        //method "submit"
+        const submit = () => {
+
+            //send data to server
+            router.put(`/apps/customers/${props.customer.id}`, {
+                //data
+                name: form.name,
+                privilege: form.privilege,
+                description: form.description
+            }, {
+                onSuccess: () => {
+                    //show success alert
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Customer updated successfully.',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                },
+            });
+
+        }
+
+        return {
+            form,
+            submit,
+        };
+
+    }
+}
+</script>
+
+<style></style>
